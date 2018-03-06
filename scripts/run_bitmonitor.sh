ead12c36063d0a77a0c55689d20297ae5f04f442
jahyeonbeak <jahyeonbeak@gmail.com>
2018-03-06 09:28:34 +0800

    Fix bug

--- scripts/run_bitmonitor.sh
+++ scripts/run_bitmonitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup python script/bitmonitor.py 1>/dev/null 2>&1 &
+nohup python bitmonitor.py 1>/dev/null 2>&1 &
 #nohup python run_update_price.py 1>/dev/null 2>&1 &
 #nohup python run_update_spread.py 1>/dev/null 2>&1 &
